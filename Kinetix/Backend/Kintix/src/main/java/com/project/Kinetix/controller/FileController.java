1e6adbc8e037d18289d3e7f75a0bc224e7b0d372
imjay05 <imjaydigambarshelke@gmail.com>
2025-12-25 11:19:04 +0530

    Kinetix update

--- Kinetix/Backend/Kintix/src/main/java/com/project/Kinetix/controller/FileController.java
+++ Kinetix/Backend/Kintix/src/main/java/com/project/Kinetix/controller/FileController.java
@@ -16,8 +16,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/files")
 @CrossOrigin(origins = {
-        "http://localhost:5500",
-        "http://localhost:3000",
+         "http://localhost:3000",
+        "http://127.0.0.1:3000"
         "*"
 })
 public class FileController {
